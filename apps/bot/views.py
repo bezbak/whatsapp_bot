f0a8e8c31333c044035bc364dd5f2608f4d43f3c
bezbak <Cemetey.2019@gmail.com>
2023-08-22 04:07:56 +0600

    new commit

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -1,3 +1,17 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from apps.bot.models import Order
+from twilio.rest import Client
+from django.conf import settings
+from core.config import account_sid1, auth_token1
+
+client = Client(account_sid1, auth_token1)
 # Create your views here.
+@api_view(['POST', "GET"])
+def test(request):
+    print(request.POST.get('Body'))
+
+@api_view(['POST', "GET"])
+def test2(request):
+    print(account_sid1)
+
+    
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from apps.bot.views import test, test2
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('incoming/', test),
+    path('test2/', test2),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
