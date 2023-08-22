3c85335e25d6cbb368c317806e9ab27ec0074dc3
bezbak <Cemetey.2019@gmail.com>
2023-08-23 03:07:03 +0600

    fixing bug with media

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -13,6 +13,8 @@ def hello_text(request):
     phone_number = request.POST.get('From')
     for i in Menu.objects.all():
         message = f"""{i.name}\n{i.description}\n{i.price}"""
+        print(i.image.url)
+        print(i.image)
         send_message(phone_number,message, i.image.url)
 
     return HttpResponse({'200':'OK'})
