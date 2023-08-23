434a64755b28e559dec65026d59d90d21c6fb370
bezbak <Cemetey.2019@gmail.com>
2023-08-23 12:42:24 +0600

    fix bug

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from apps.bot.models import Menu
+from apps.bot.models import Menu, Order, MenuToOrder
 # Register your models here.
 admin.site.register(Menu)
+admin.site.register(Order)
+admin.site.register(MenuToOrder)
