ed44b7f5a58d1cfd7ad2c23cfd76f88ddd42c06c
bezbak <Cemetey.2019@gmail.com>
2023-08-23 13:50:31 +0600

    new bot

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -101,6 +101,7 @@ def del_order(phone_number, text):
         send_message('whatsapp:+996557500113', message=message)
         send_message(phone_number, message='Мы приняли ваш заказ, ожидайте ответа')
     else:
+        step =2
         set_order(phone_number, text)
 def get_menu(phone_number, text):
     global step
