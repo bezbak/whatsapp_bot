b9f5f1cca8d8e7491b052cf75f88ede4f9d8c39b
bezbak <Cemetey.2019@gmail.com>
2023-08-23 13:47:57 +0600

    new bot

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -72,7 +72,7 @@ def set_order(phone_number, text):
     message = f"""✅✅✅ВАШ ЗАКАЗ✅✅✅\n\n{', '.join(' '.join((i.dish.name,'Количество:',str(i.count))) for i in order.one_order.all())}\n\nСумма:{order.sum_of_order}\n\n     🔥🔥🔥ДОБАВИТЬ ЕЩЁ, НАПИШИТЕ НОМЕР БЛЮДА🔥🔥🔥  \n\n🤝🤝🤝ОФОРМИТЬ ЗАКАЗ🤝🤝🤝 отправьте «ОК»\n\nНапишите 'нет' чтобы отменить выбор или 'Отмена' чтобы отменить заказ"""
     is_order = True
     send_message(phone_number,message)
-    step+=1
+    step=4
 
 def del_order(phone_number, text):
     global step
