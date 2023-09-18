904c198e07cf0a86526114bac773eafa7047b701
bezbak <Cemetey.2019@gmail.com>
2023-09-18 21:46:53 +0600

    fafea

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from apps.bot.models import Menu, Order, MenuToOrder
+from apps.bot.models import Menu, Order, MenuToOrder, Category
 # Register your models here.
 admin.site.register(Menu)
 admin.site.register(Order)
 admin.site.register(MenuToOrder)
+admin.site.register(Category)
