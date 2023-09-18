42641f2f3e4e05a7ee8f31d80145285952ffd96a
bezbak <Cemetey.2019@gmail.com>
2023-09-18 21:58:17 +0600

    fafea

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from apps.bot.models import Menu, Order, MenuToOrder, Category
 # Register your models here.
+
+@admin.action(description="Добавить в черновик")
+def make_unpublished(modeladmin, request, queryset):
+    queryset.update(draft = True)
+
+@admin.action(description="Убрать из черновика")
+def make_published(modeladmin, request, queryset):
+    queryset.update(draft = True)
+
 class MenuAdmin(admin.ModelAdmin):
     list_filter = ["draft"]
+    actions = [make_published, make_unpublished]
+
 admin.site.register(Menu, MenuAdmin)
 admin.site.register(Order)
 admin.site.register(MenuToOrder)
